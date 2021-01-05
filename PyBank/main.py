4ccb1ee12a26e6780d6c0f41b233ae3cf89d7247
Bang <banggabr@amazon.com>
2021-01-04 20:47:19 -0600

    python homework update

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,10 +4,17 @@ budget_csv = os.path.join('..', 'Resources', 'budget_data.csv')
 
 # Read in csv file
 with open(budget_csv, 'r') as csvfile:
-    # Split the data on commas 
+    # Use csvreader functionality to specify variable to hold csv contents, 
     csvreader = csv.reader(csvfile, delimiter=',')
+    # Skip header
     header = next(csvreader)
 
+# 
+    
+    
     # Print header then print border using next line functionality
     print(f'Financial Analysis'+'\n')
     print(f'------------------------------------'+'\n')
+
+    
+# Total months
