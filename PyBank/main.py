4650901ec28463be3ae54b5ba648ae63ed825f50
Bang <banggabr@amazon.com>
2021-01-04 19:23:54 -0600

    python homework update

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,13 +7,7 @@ with open(budget_csv, 'r') as csvfile:
     # Split the data on commas 
     csvreader = csv.reader(csvfile, delimiter=',')
     header = next(csvreader)
-    
-    
-    
-    
-    
-    
+
     # Print header then print border using next line functionality
     print(f'Financial Analysis'+'\n')
     print(f'------------------------------------'+'\n')
-    
